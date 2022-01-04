d9a1c91f39068f19af0b6f5e4ecc25141cce2593
Jay-flow <kimpetro153@gmail.com>
2022-01-04 16:00:03 +0900

    Change the bundler to Vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   extends: '@dooboo/eslint-config-svelte',
   rules: {
     '@typescript-eslint/no-unsafe-call': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
 };
--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('precss'),
     require('postcss-import'),
-    require('postcss-cssnext'),
     require('postcss-preset-env'),
     require('postcss-nested'),
     require('postcss-custom-media'),
