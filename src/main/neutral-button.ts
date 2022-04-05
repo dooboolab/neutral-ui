64516766bcaffead37a4a45837fea805d176fb0e
DevYuns <ydhun93@gmail.com>
2022-04-05 15:07:10 +0900

    Create basic button ui

---
+++ src/main/neutral-button.ts
@@ -0,0 +1,140 @@
+import {LitElement, TemplateResult, css, html} from 'lit';
+import {customElement, property, query} from 'lit/decorators.js';
+
+import {colors} from '../colors';
+
+@customElement('neutral-button')
+export class NeutralButton extends LitElement {
+  static styles = [
+    colors,
+    css`
+      :host {
+        display: inline-block;
+      }
+
+      :host button {
+        background-color: var(--button-color, #000);
+        color: var(--text-color, #fff);
+        border: 1px solid var(--border-color, #000);
+        width: 6em;
+        height: 3em;
+        border-radius: 2em;
+        cursor: pointer;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+      }
+
+      :host(.small) button {
+        font-size: 0.75rem;
+      }
+
+      :host(.medium) button {
+        font-size: 1rem;
+      }
+
+      :host(.large) button {
+        font-size: 1.125rem;
+      }
+
+      :host(.primary) button {
+        background-color: var(--primary);
+        color: var(--white);
+        border: none;
+      }
+
+      :host(.secondary) button {
+        background-color: var(--secondary);
+        color: var(--black);
+        border: none;
+      }
+
+      :host(.success) button {
+        background-color: var(--success);
+        color: var(--black);
+        border: none;
+      }
+
+      :host(.danger) button {
+        background-color: var(--danger);
+        color: var(--white);
+        border: none;
+      }
+
+      :host(.warning) button {
+        background-color: var(--warning);
+        color: var(--black);
+        border: none;
+      }
+
+      :host(.info) button {
+        background-color: var(--info);
+        color: var(--black);
+        border: none;
+      }
+
+      :host(.light) button {
+        background-color: var(--light);
+        color: var(--black);
+        border: none;
+      }
+
+      :host(.disabled) button {
+        background-color: var(--disabled);
+        color: var(--black);
+        border: none;
+      }
+
+      /* :host(.outlined) button {
+        background-color: var(--white);
+        color: var(--black);
+        border: 1px solid var(--warning);
+      } */
+
+      .loader {
+        border: 2px solid var(--background-color, #fff);
+        border-top: 2px solid var(--text-color, #000);
+        border-radius: 50%;
+        width: 1rem;
+        height: 1rem;
+        animation: spin 1s linear infinite;
+      }
+
+      @keyframes spin {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    `,
+  ];
+
+  @property({type: Boolean})
+  loading = false;
+
+  @property({type: String})
+  size = 'small';
+
+  @property({type: Boolean})
+  outlined = false;
+
+  protected render(): TemplateResult {
+    return html`
+      <button>
+        <slot name="left-element"></slot>
+        ${this.loading ? html`<div class="loader" />` : html`<slot />`}
+        <slot name="right-element"></slot>
+      </button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'neutral-button': NeutralButton;
+  }
+}
--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,10 +1,22 @@
-import '../src/main/my-element';
+import '../src/main/neutral-button';
 
 import {TemplateResult, html} from 'lit';
 
 export default {
-  title: 'MyElement',
-  component: 'my-element',
+  title: 'Button',
+  component: 'neutral-button',
+  argTypes: {
+    textColor: {control: 'color'},
+    buttonColor: {control: 'color'},
+    borderColor: {control: 'color'},
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: {type: 'select'},
+    },
+    loading: {control: 'boolean'},
+    click: {action: 'clicked'},
+  },
+  label: {control: 'text'},
 };
 
 interface Story<T> {
@@ -13,7 +25,93 @@ interface Story<T> {
   argTypes?: Record<string, unknown>;
 }
 
-const Template: Story<TemplateResult> = () =>
-  html`<my-element child="test component"></my-element> `;
+interface ArgTypes {
+  textColor?: string;
+  buttonColor?: string;
+  borderColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  loading?: boolean;
+  onClick?: {};
+}
 
-export const Regular = Template.bind({});
+const template: Story<ArgTypes> = ({
+  textColor,
+  buttonColor,
+  borderColor,
+  loading,
+  size,
+}) =>
+  html` <neutral-button
+    class=${size}
+    style="--text-color: ${textColor}; --button-color: ${buttonColor}; --border-color: ${borderColor}"
+    .loading=${loading}
+  >
+    Button
+  </neutral-button>`;
+
+export const Button: Story<ArgTypes> = (args) =>
+  html`
+    <style>
+      .container {
+        margin: 0 20px;
+      }
+
+      section {
+        border-bottom: 1px solid #000;
+        padding: 30px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .button-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 25px;
+      }
+    </style>
+
+    <main class="container">
+      <section>
+        <h2>Demo</h2>
+        <div>${template({...args})}</div>
+      </section>
+      <section>
+        <h2>Basic Styles</h2>
+        <div class="button-wrapper">
+          <neutral-button class="medium primary">Primary</neutral-button>
+          <neutral-button class="medium secondary">Secondary</neutral-button>
+          <neutral-button class="medium success">Success</neutral-button>
+          <neutral-button class="medium danger">Danger</neutral-button>
+          <neutral-button class="medium warning">Warning</neutral-button>
+          <neutral-button class="medium info">Info</neutral-button>
+          <neutral-button class="medium light">Light</neutral-button>
+          <neutral-button class="medium disabled">Disabled</neutral-button>
+        </div>
+      </section>
+      <section>
+        <h2>Outlined Styles</h2>
+        <div class="button-wrapper">
+          <neutral-button class="medium primary">Primary</neutral-button>
+          <neutral-button class="medium secondary">Secondary</neutral-button>
+          <neutral-button class="medium success">Success</neutral-button>
+          <neutral-button class="medium danger">Danger</neutral-button>
+          <neutral-button class="medium warning">Warning</neutral-button>
+          <neutral-button class="medium info">Info</neutral-button>
+          <neutral-button class="medium light">Light</neutral-button>
+          <neutral-button class="medium disabled">Disabled</neutral-button>
+        </div>
+      </section>
+      <section>
+        <h2>Aspect of sizes</h2>
+        <div class="button-wrapper">
+          <neutral-button class="large">button</neutral-button>
+          <neutral-button class="medium">button</neutral-button>
+          <neutral-button class="small">button</neutral-button>
+        </div>
+      </section>
+    </main>
+  `;
