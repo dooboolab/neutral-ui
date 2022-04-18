937d388ef5e444d7be9dab5f1f7c506e2faf08f8
DevYuns <ydhun93@gmail.com>
2022-04-18 16:01:32 +0900

    Modify stories

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -14,6 +14,7 @@ export default {
       control: {type: 'select'},
     },
     loading: {control: 'boolean'},
+    classNames: {control: 'text'},
     click: {action: 'clicked'},
   },
   label: {control: 'text'},
@@ -30,8 +31,9 @@ interface ArgTypes {
   buttonColor?: string;
   borderColor?: string;
   size?: 'small' | 'medium' | 'large';
+  classNames?: string;
   loading?: boolean;
-  onClick?: {};
+  onClick?: () => void;
 }
 
 const template: Story<ArgTypes> = ({
@@ -40,17 +42,28 @@ const template: Story<ArgTypes> = ({
   borderColor,
   loading,
   size,
+  onClick,
+  classNames,
 }) =>
   html` <neutral-button
-    class=${size}
+    class=${`${classNames} ${size}`}
     style="--text-color: ${textColor}; --button-color: ${buttonColor}; --border-color: ${borderColor}"
     .loading=${loading}
+    @click=${onClick}
   >
     Button
   </neutral-button>`;
 
-export const Button: Story<ArgTypes> = (args) =>
-  html`
+const facebookButton = template.bind({});
+facebookButton.args = {
+  loading: true,
+  onClick: () => {
+    console.log('test');
+  },
+};
+
+export const Button: Story<ArgTypes> = (args) => {
+  return html`
     <style>
       .container {
         margin: 0 20px;
@@ -66,10 +79,15 @@ export const Button: Story<ArgTypes> = (args) =>
         align-items: center;
       }
 
+      section h2 {
+        margin-bottom: 3rem;
+      }
+
       .button-wrapper {
         display: flex;
         justify-content: center;
         align-items: center;
+        flex-wrap: wrap;
         gap: 25px;
       }
     </style>
@@ -77,7 +95,7 @@ export const Button: Story<ArgTypes> = (args) =>
     <main class="container">
       <section>
         <h2>Demo</h2>
-        <div>${template({...args})}</div>
+        ${template({...args})}
       </section>
       <section>
         <h2>Basic Styles</h2>
@@ -92,26 +110,64 @@ export const Button: Story<ArgTypes> = (args) =>
           <neutral-button class="medium disabled">Disabled</neutral-button>
         </div>
       </section>
+
       <section>
         <h2>Outlined Styles</h2>
         <div class="button-wrapper">
-          <neutral-button class="medium primary">Primary</neutral-button>
-          <neutral-button class="medium secondary">Secondary</neutral-button>
-          <neutral-button class="medium success">Success</neutral-button>
-          <neutral-button class="medium danger">Danger</neutral-button>
-          <neutral-button class="medium warning">Warning</neutral-button>
-          <neutral-button class="medium info">Info</neutral-button>
-          <neutral-button class="medium light">Light</neutral-button>
-          <neutral-button class="medium disabled">Disabled</neutral-button>
+          <neutral-button class="medium primary outlined">
+            Primary
+          </neutral-button>
+          <neutral-button class="medium secondary outlined">
+            Secondary
+          </neutral-button>
+          <neutral-button class="medium success outlined">
+            Success
+          </neutral-button>
+          <neutral-button class="medium danger outlined">
+            Danger
+          </neutral-button>
+          <neutral-button class="medium warning outlined">
+            Warning
+          </neutral-button>
+          <neutral-button class="medium info outlined"> Info </neutral-button>
+          <neutral-button class="medium light outlined">Light</neutral-button>
+          <neutral-button class="medium disabled outlined">
+            Disabled
+          </neutral-button>
         </div>
       </section>
+
       <section>
         <h2>Aspect of sizes</h2>
         <div class="button-wrapper">
-          <neutral-button class="large">button</neutral-button>
-          <neutral-button class="medium">button</neutral-button>
-          <neutral-button class="small">button</neutral-button>
+          <neutral-button class="large">Large</neutral-button>
+          <neutral-button class="medium">Medium</neutral-button>
+          <neutral-button class="small">Small</neutral-button>
+        </div>
+      </section>
+
+      <section>
+        <h2>Adding elements</h2>
+        <div class="button-wrapper">
+          <neutral-button class="large outlined">
+            <img
+              slot="left-element"
+              src="../assets/google.png"
+              style="margin-right: 20px;"
+            />
+            GOOGLE SIGN IN
+          </neutral-button>
+          <neutral-button class="large">
+            <img
+              slot="left-element"
+              src="../assets/facebook.png"
+              style="margin-right: 20px;"
+            />
+            <span>FACEBOOK SIGN IN</span>
+          </neutral-button>
+          ${facebookButton({...args})}
         </div>
       </section>
     </main>
   `;
+};
